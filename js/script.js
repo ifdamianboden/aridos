14ffaf01775eaad398ed700b6167db9826192d1c
ifdamianboden <ifdamianelsebodeman@gmail.com>
2025-03-23 23:14:55 -0300

    estilos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    console.log("Clase actual:", x.className);
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
+    console.log("Nueva clase:", x.className);
 }
